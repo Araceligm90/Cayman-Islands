be6f937dce494f0658c14ef2729c9f2dea953b1a
Araceligm90 <garcia.araceli90@gmail.com>
2022-09-20 07:20:12 -0500

    javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+let userName = prompt("What is your name?");
+document.write("Hello ", userName);
+
+
+let userAge = prompt("How old are you?");
+document.write("I am 31 years old!");
+
+
+let userState = prompt("Which state do you live in?");
+
+if (userState.toLowerCase() == "Massachusetts")
+document.write("I have lived there too!")
+else {
+    document.write("I would like to live there one day too!");
+}
